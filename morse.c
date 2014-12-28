2ad81e8c76bd8a3556115f8622bca8f9277db974
James Riley Wynne III <wynnejr@ornl.gov>
2014-12-28 17:57:17 -0500

    Added morse code lookup and output

--- morse.c
+++ morse.c
@@ -1,39 +1,96 @@
 #include  <msp430g2553.h>
 
-volatile int i = 0;                         
+volatile unsigned int i = 0;                         
 
-void dot(int num = 1)
+void wait(unsigned int iter)
 {
-  for(int a = 0; a < num; a++)
-  {
-    P1OUT ^= 0x01;
-    for(i=0; i< 10000; i++); 
-    P1OUT ^= 0x01;
-    for(i=0; i< 10000; i++); 
-  }
-  for(i=0; i< 30000; i++); 
+  for (i = 0; i < iter; i++);
 }
 
-void dash(int num = 1)
+void dot()
 {
-  for(int a = 0; a < num; a++)
+  P1OUT ^= BIT0;
+  wait(10000);
+  P1OUT ^= BIT0;
+  wait(30000);
+}
+
+void dash()
+{
+  P1OUT ^= BIT0;
+  wait(30000);
+  P1OUT ^= BIT0;
+  wait(30000);
+}
+
+void getMChar(char c)
+{
+  char* morse_table[] = 
+  { 
+    ".-" , // A
+    "-...", // B
+    "-.-.", // C
+    "-..", // D
+    ".", // E
+    "..-.", // F
+    "--." , // G
+    "....", // H
+    ".."  , // I
+    ".---", // J
+    ".-.-", // K
+    ".-..", // L
+    "--", // M
+    "-.", // N
+    "---", // O
+    ".--.", // P
+    "--.-", // Q
+    ".-.", // R
+    "...", // S
+    "-", // T
+    "..-", // U
+    "...-", // V
+    ".--", // W
+    "-..-", // X
+    "-.--", // Y
+    "--.." // Z  
+  };
+  int val = (int)c;
+  int idx = val - 65;
+  char* mChar = morse_table[idx];  
+  char temp = ' ';
+  int a = 0;
+  while(temp != '\0')
   {
-    P1OUT ^= 0x01;
-    for(i=0; i< 20000; i++); 
-    P1OUT ^= 0x01;
-    for(i=0; i< 20000; i++); 
+    temp = mChar[a];
+    if (temp == '.')
+      dot();
+    else if (temp == '-')
+      dash();
+    a++;
   }
-  for(i=0; i< 30000; i++); 
+}
+
+void morseStrOut(char* str)
+{
+  char c = ' ';
+  int a = 0;
+  while (c != '\0')
+  {
+    c = str[a];
+    getMChar(c);
+    a++;
+  }
 }
 
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                //Stop watchdog timer 
-  P1DIR = 0x01;
-  P1OUT = 0;
-  dot(3);
-  dash(3);
-  dot(3);
+  P1DIR = BIT0;                            //Set the direction of P1.0 (LED1)
+  P1OUT = 0;                               //Set the P1OUT register to a known state
+  morseStrOut("Jake");
+  P1DIR = BIT6;                            //Turn on the green LED (P1.6) just for debug purposes
+  P1OUT = BIT6;
   for(;;);
 }
+
 
