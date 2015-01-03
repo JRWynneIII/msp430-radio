fdfef6189c9e81d5e8a60cebe559d18638847676
James Riley Wynne III <wynnejr@ornl.gov>
2015-01-03 01:15:31 -0500

    Fixed weird bug that would loop back to A after idx>M? Made global. Not sure why it worked

--- morse.c
+++ morse.c
@@ -24,49 +24,79 @@ void dash()
   wait(30000);
 }
 
+char* morseChar[] = 
+{ 
+  ".-" , // A
+  "-...", // B
+  "-.-.", // C
+  "-..", // D
+  ".", // E
+  "..-.", // F
+  "--." , // G
+  "....", // H
+  ".."  , // I
+  ".---", // J
+  "-.-", // K
+  ".-..", // L
+  "--", // M
+  "-.", // N
+  "---", // O
+  ".--.", // P
+  "--.-", // Q
+  ".-.", // R
+  "...", // S
+  "-", // T
+  "..-", // U
+  "...-", // V
+  ".--", // W
+  "-..-", // X
+  "-.--", // Y
+  "--..", // Z  
+  "-----", //0
+  ".----", //1
+  "..---", //2
+  "...--", //3
+  "....-", //4
+  ".....", //5
+  "-....", //6
+  "--...", //7
+  "---..", //8
+  "----.", //9
+  ".-.-.-" //.
+};
+
 void getMChar(char c)
 {
-  char* morseChar[] = 
-  { 
-    ".-" , // A
-    "-...", // B
-    "-.-.", // C
-    "-..", // D
-    ".", // E
-    "..-.", // F
-    "--." , // G
-    "....", // H
-    ".."  , // I
-    ".---", // J
-    "-.-", // K
-    ".-..", // L
-    "--", // M
-    "-.", // N
-    "---", // O
-    ".--.", // P
-    "--.-", // Q
-    ".-.", // R
-    "...", // S
-    "-", // T
-    "..-", // U
-    "...-", // V
-    ".--", // W
-    "-..-", // X
-    "-.--", // Y
-    "--.." // Z  
-  };
-  int val = (int)c;
-  int idx = val - 65;
-  char* mChar = morseChar[idx];  
+  int idx = 0;
+  int val = (int)c; 
+  char* mChar;
+  if (c >= '0' && c <= '9')
+  {
+    val -= 48;
+    idx = (val + 26);
+    mChar = morseChar[idx];
+  }
+  else
+  {
+    idx = val - 65;
+    mChar = morseChar[idx];  
+  }
+  if (c == '.')
+    mChar = morseChar[36];
   char temp = ' ';
   int a = 0;
   while(temp != '\0')
   {
     temp = mChar[a];
-    if (temp == '.')
-      dot();
-    else if (temp == '-')
-      dash();
+    switch(temp)
+    {
+      case '.': 
+        dot();
+        break;
+      case '-':
+        dash();
+        break;
+    }
     a++;
   }
   wait(50000);
@@ -76,11 +106,12 @@ void morseStrOut(const char* str)
 {
   char c = ' ';
   int a = 0;
+  c = str[a];
   while (c != '\0')
   {
-    c = str[a];
     getMChar(c);
     a++;
+    c = str[a];
   }
 }
 
@@ -89,7 +120,7 @@ int main(void)
   WDTCTL = WDTPW + WDTHOLD;                //Stop watchdog timer 
   P1DIR = BIT0;                            //Set the direction of P1.0 (LED1)
   P1OUT = 0;                               //Set the P1OUT register to a known state
-  const char* str = "JAKE";
+  const char* str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.";
   morseStrOut(str);
   P1DIR = BIT6;                            //Turn on the green LED (P1.6) just for debug purposes
   P1OUT = BIT6;
