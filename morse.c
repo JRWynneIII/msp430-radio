dfb4046da050f0bc8344797c5cd32a65461d29a9
James Riley Wynne III <wynnejr@ornl.gov>
2015-01-03 15:46:53 -0500

    Added code to get temp from ADC

--- morse.c
+++ morse.c
@@ -1,4 +1,4 @@
-#include  <msp430g2553.h>
+#include <msp430g2553.h>
 
 volatile unsigned int i = 0;                         
 
@@ -115,14 +115,76 @@ void morseStrOut(const char* str)
   }
 }
 
+void initADCTemp()
+{
+  ADC10CTL0=SREF_1 + REFON + ADC10ON + ADC10SHT_3;  //1.5V reference, ref on, 64 clocks per sample
+  ADC10CTL1=INCH_10 + ADC10DIV_3;                   //temp sensor is at 10 and clock/4
+}
+
+int getTemp()
+{
+  int t=0;
+  __delay_cycles(1000);                             //wait for ref to settle
+  ADC10CTL0 |= ENC + ADC10SC;                       //enable and start conversion
+  while(ADC10CTL0 & BUSY);                          //Wait while busy
+  t=ADC10MEM;                                       //store temp in t
+  ADC10CTL0 &= ~ENC;                                //And it with NOT Enc to disable adc conversion
+  return (int)((t*27069L - 18169625L) >> 16);       //Convert value 
+}
+
+void itoa( int val, char *s )
+{
+  char *p;
+  unsigned char d, i;
+  unsigned char zero;
+  unsigned int test;
+  unsigned int uval = val;
+  
+
+  p = s;
+  
+  if( val < 0 ){
+    uval = -val;
+    *p++ = '-';
+  }
+
+  zero = 1;
+
+  i = 4;
+  do{
+    i--;   
+    if ( i==0) test = 10;
+    else if ( i==1) test=100;
+    else if ( i==2) test=1000;
+    else test=10000;
+
+    for( d = '0'; uval >= test; uval -= test )
+    {
+      d++;
+      zero = 0;
+    }
+    if( zero == 0 )
+      *p++ = d ;
+  }while( i );
+
+  *p++ = (unsigned char)uval + '0';
+}
+
 int main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                //Stop watchdog timer 
   P1DIR = BIT0;                            //Set the direction of P1.0 (LED1)
   P1OUT = 0;                               //Set the P1OUT register to a known state
-  const char* str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.";
-  morseStrOut(str);
-  P1DIR = BIT6;                            //Turn on the green LED (P1.6) just for debug purposes
-  P1OUT = BIT6;
-  for(;;);
+  volatile int temp = 0;
+  initADCTemp();
+  char* str;
+  while(1)
+  {
+    __delay_cycles(500);
+    temp = getTemp();
+    itoa(temp,str);
+    dot();
+    __delay_cycles(500);
+    morseStrOut(str);
+  }
 }
